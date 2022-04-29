c439311ab5693c8f87f1a8d038e690937aee9deb
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-29 23:16:56 +0400

    update bot.py

--- bot.py
+++ bot.py
@@ -300,59 +300,6 @@ async def mentionall(tagadmin):
 		sleep(0.5)
 
 
-
-### istatistik 
-
-@client.on(events.NewMessage())
-async def mentionalladmin(event):
-  global grup_sayi
-  if event.is_group:
-    if event.chat_id in grup_sayi:
-      pass
-    else:
-      grup_sayi.append(event.chat_id)
-
-@client.on(events.NewMessage(pattern='^/botstatik ?(.*)'))
-async def son_durum(event):
-    global anlik_calisan,grup_sayi,ozel_list
-    sender = await event.get_sender()
-    if sender.id not in ozel_list:
-      return
-    await event.respond(f"**Gece kuşu Tagger İstatistikleri 🤖**\n\nToplam Grup: `{len(grup_sayi)}`\nAnlık Çalışan Grup: `{len(anlik_calisan)}`")
-
-
-### brokcast modülü
-
-@client.on(events.NewMessage(pattern='^/botreklam ?(.*)'))
-async def duyuru(event):
- 
-  global grup_sayi,ozel_list
-  sender = await event.get_sender()
-  if sender.id not in ozel_list:
-    return
-  reply = await event.get_reply_message()
-  await event.respond(f"Toplam {len(grup_sayi)} Gruba'a mesaj gönderiliyor...")
-  for x in grup_sayi:
-    try:
-      await client.send_message(x,f"**📣 Sponsor**\n\n{reply.message}")
-    except:
-      pass
-  await event.respond(f"Gönderildi.")
-
-
-#### botcum modülü
-
-@app.on_message(filters.user(5074483091) & filters.command(["botcum"], ["."]))
-def admin(_, message: Message):
-    message.reply(f"__Biricik Sahibim Gelmiş Hoşgeldin Efendim 💋 Muck__")
-
-
-
-print(">> Bot çalıyor merak etme 🚀 @lucimarka bilgi alabilirsin <<")
-client.run_until_disconnected()
-
-
-
 ### istatistik 
 
 @client.on(events.NewMessage())
