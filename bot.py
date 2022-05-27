77a0a6afae206639113a22566985277042670c64
Imran95942 <halil-223@mail.ru>
2022-05-27 11:51:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,7 +111,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
       if event.chat_id not in anlik_calisan:
-        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**❌")
+        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**")
         return
       if usrnum == 5:
         await client.send_message(event.chat_id, usrtxt, reply_to=msg)
@@ -159,7 +159,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"👥 - [{usr.first_name}](tg://user?id={usr.id}) \n"
       if event.chat_id not in anlik_calisan:
-        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**❌")
+        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**")
         return
       if usrnum == 14:
         await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
@@ -224,7 +224,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"**👤 - [{usr.first_name}](tg://user?id={usr.id}) \n**"
       if event.chat_id not in anlik_calisan:
-        await event.respond("**Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**❌**")
+        await event.respond("**Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**")
         return
       if usrnum == 1:
         await client.send_message(event.chat_id, f"{usrtxt} {msg}")
@@ -242,7 +242,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"👤 - [{usr.first_name}](tg://user?id={usr.id}) \n"
       if event.chat_id not in anlik_calisan:
-        await event.respond("Операция успешно остановлена\n\n** https://t.me/+NSd1gUOdDbgxMGUy**❌**")
+        await event.respond("Операция успешно остановлена\n\n** https://t.me/+NSd1gUOdDbgxMGUy**")
         return
       if usrnum == 1:
         await client.send_message(event.chat_id, usrtxt, reply_to=msg)
@@ -327,7 +327,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.user.id}) "
       if event.chat_id not in anlik_calisan:
-        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**❌")
+        await event.respond("Операция успешно остановлена\n\n**https://t.me/+NSd1gUOdDbgxMGUy**")
         return
       if usrnum == 1:
         await client.send_message(event.chat_id, usrtxt, reply_to=msg)
